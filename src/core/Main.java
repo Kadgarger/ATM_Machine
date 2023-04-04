d2362351583d4174c1fdf3638e1bdc7e7ac24c9e
Kadgarger <necronet.george.danilov@gmail.com>
2023-04-04 04:58:16 +0300

    add scanner and its check

--- src/core/Main.java
+++ src/core/Main.java
@@ -1,7 +1,21 @@
 package core;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        int atmnumber = 1337228;
+        int atmpin = 1488;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter ATM Number: ");
+        int atmNumber = in.nextInt();
+        System.out.print("Enter PIN: ");
+        int PIN = in.nextInt();
+        if ((atmnumber==atmNumber)&&(atmpin==PIN))
+            System.out.println("Validation done");
+        else {
+            System.out.println("Access denied");
+            System.exit(0);
+        }
     }
 }
