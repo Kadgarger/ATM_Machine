df1f0cc556982c09e23ba0a1cd291f5e8f2a5497
Kadgarger <necronet.george.danilov@gmail.com>
2023-04-04 05:43:03 +0300

    add chose and 5 and null variants

--- src/core/Main.java
+++ src/core/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Welcome to ATM");
         int atmnumber = 1337228;
         int atmpin = 1488;
         Scanner in = new Scanner(System.in);
@@ -11,8 +12,24 @@ public class Main {
         int atmNumber = in.nextInt();
         System.out.print("Enter PIN: ");
         int PIN = in.nextInt();
-        if ((atmnumber==atmNumber)&&(atmpin==PIN))
-            System.out.println("Validation done");
+        if ((atmnumber==atmNumber)&&(atmpin==PIN)){
+            while (true) {
+                System.out.print("1. View available balance\n2. Withdraw amount\n3. Deposit amount\n4. View ministatement\n5. Exit");
+                System.out.println("Enter choice:");
+                int ch = in.nextInt();
+                if (ch == 1) {}
+                    else if (ch == 2) {}
+                    else if (ch == 3) {}
+                    else if (ch == 4) {}
+                    else if (ch == 5) {
+                        System.out.println("Collect you ATM card\nThank you. You have chosen, or been chosen, to use to one of our finest remaining urban ATM");
+                        System.exit(0);
+                    }
+                    else{
+                         System.out.println("Please enter correct choose");
+                }
+            }
+        }
         else {
             System.out.println("Access denied");
             System.exit(0);
