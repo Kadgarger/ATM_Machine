b3dcae7b24a46c11546ddaa296095201d9737660
Kadgarger <necronet.george.danilov@gmail.com>
2023-04-04 06:29:50 +0300

    add 1-3 chooses

--- src/core/AtmOperation.java
+++ src/core/AtmOperation.java
@@ -1,4 +1,34 @@
 package core;
 
-public class AtmOperation {
+public class AtmOperation implements AtmOperationInteface{
+    ATM atm = new ATM();
+
+    @Override
+    public void viewBalance() {
+        System.out.println("Available balance is: " + atm.getBalance());
+    }
+
+    @Override
+    public void withdrawAmount(double withdrawAmount) {
+        if (withdrawAmount <= atm.getBalance()) {
+            System.out.println("Collect the cash " + withdrawAmount);
+            atm.setBalance(atm.getBalance() - withdrawAmount);
+            viewBalance();
+        }
+        else {
+            System.out.println("You try to take more cash than you balance. Input correct number");
+        }
+    }
+
+    @Override
+    public void depositAmount(double depositAmount) {
+        System.out.println(depositAmount + " Deposit successfully");
+        atm.setBalance(atm.getBalance() + depositAmount);
+        viewBalance();
+    }
+
+    @Override
+    public void viewMiniStatement() {
+
+    }
 }
--- src/core/Main.java
+++ src/core/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Welcome to ATM");
+        AtmOperationInteface op = new AtmOperation();
+        System.out.println("Welcome to ATM");
         int atmnumber = 1337228;
         int atmpin = 1488;
         Scanner in = new Scanner(System.in);
@@ -14,12 +15,20 @@ public class Main {
         int PIN = in.nextInt();
         if ((atmnumber==atmNumber)&&(atmpin==PIN)){
             while (true) {
-                System.out.print("1. View available balance\n2. Withdraw amount\n3. Deposit amount\n4. View ministatement\n5. Exit");
-                System.out.println("Enter choice:");
+                System.out.print("1. View available balance\n2. Withdraw amount\n3. Deposit amount\n4. View ministatement\n5. Exit\n");
+                System.out.print("Enter choice: ");
                 int ch = in.nextInt();
-                if (ch == 1) {}
-                    else if (ch == 2) {}
-                    else if (ch == 3) {}
+                if (ch == 1) {op.viewBalance();}
+                    else if (ch == 2) {
+                        System.out.println("Enter amount to withdraw");
+                        int withdrawAmount = in.nextInt();
+                        op.withdrawAmount(withdrawAmount);
+                }
+                    else if (ch == 3) {
+                        System.out.println("Enter amount of deposit");
+                        double depositAmount = in.nextDouble();
+                        op.depositAmount(depositAmount);
+                }
                     else if (ch == 4) {}
                     else if (ch == 5) {
                         System.out.println("Collect you ATM card\nThank you. You have chosen, or been chosen, to use to one of our finest remaining urban ATM");
